0f39d3559e03967518ba7eb902a925d3312f9729
DanceMore <dancemore@protonmail.com>
2023-09-07 20:43:56 -0700

    use clap not argv

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,60 @@
-use std::env;
+extern crate clap;
+use clap::{App, Arg};
 use std::net::{ToSocketAddrs, UdpSocket};
 
 fn main() -> std::io::Result<()> {
-    // default values
-    let mut host = "localhost";
-    let mut port = 27960;
-    let mut rconpass = "password";
-    let mut buf = [0u8; 2084];
+    // Define the command-line interface using clap
+    let matches = App::new("RCON Client")
+        .arg(Arg::with_name("host")
+            .long("host")
+            .value_name("HOST")
+            .default_value("localhost")
+            .help("Server hostname or IP address"))
+        .arg(Arg::with_name("port")
+            .long("port")
+            .value_name("PORT")
+            .default_value("27960")
+            .help("Server port"))
+        .arg(Arg::with_name("rconpass")
+            .long("rconpass")
+            .value_name("PASSWORD")
+            .default_value("password")
+            .help("RCON password"))
+        .arg(Arg::with_name("COMMAND")
+            .help("RCON command to send")
+            .required(true)
+            .index(1))
+        .get_matches();
 
-    // parse command-line arguments
-    let args: Vec<String> = env::args().collect();
-    for i in 1..args.len() {
-        match args[i].as_str() {
-            "--host" => {
-                host = &args[i + 1];
-            }
-            "--port" => {
-                port = args[i + 1].parse().unwrap();
-            }
-            "--rconpass" => {
-                rconpass = &args[i + 1];
-            }
-            _ => (),
-        }
-    }
+    // Extract values from command-line arguments
+    let host = matches.value_of("host").unwrap();
+    let port = matches.value_of("port").unwrap().parse::<u16>().unwrap();
+    let rconpass = matches.value_of("rconpass").unwrap();
+    let command = matches.value_of("COMMAND").unwrap();
 
     let addr = (host, port).to_socket_addrs()?.next().unwrap();
 
-    // packet header
+    // Packet header
     let header = b"\xff\xff\xff\xffrcon ";
 
-    // open socket to speak to server
+    // Open socket to speak to the server
     let udp_sock = UdpSocket::bind("0.0.0.0:0")?;
 
-    // the meat
-    let command = args.last().unwrap().as_str();
+    // Construct the RCON packet
     let mut data_out = Vec::new();
     data_out.extend_from_slice(header);
     data_out.extend_from_slice(rconpass.as_bytes());
     data_out.push(b' ');
     data_out.extend_from_slice(command.as_bytes());
+
     if udp_sock.send_to(&data_out, &addr).is_ok() {
         println!("{}", String::from_utf8_lossy(&data_out));
     }
 
+    let mut buf = [0u8; 2084];
     let (amt, _src) = udp_sock.recv_from(&mut buf)?;
     let data_in = &buf[..amt];
+
     if data_in.is_empty() {
         println!("no response from server");
     } else {
